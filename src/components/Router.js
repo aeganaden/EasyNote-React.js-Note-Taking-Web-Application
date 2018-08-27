5e03f201851bb17317442cf09c26dc399f0333d1
Angelo Ganaden Jr <angelojrganaden@gmail.com>
2018-08-27 16:44:18 +0800

    testing with hashrouter

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Login from "./Login";
 import App from "../App";
 import NotFound from "./NotFound";
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/notes/:userKey" component={App} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
